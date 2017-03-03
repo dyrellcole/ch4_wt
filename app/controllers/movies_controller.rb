12438014d434fdcfa2287ff23bd6e8c70e75219d
Dyrell Cole <dcole27@uncc.edu>
2017-03-03 03:21:44 +0000

    Done with section 4.7.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,30 +3,31 @@ class MoviesController < ApplicationController
   def index
     @movies = Movie.all
   end
-end
 
-#in app/controllers/movies_controller.rb
 
-def show
-  id = params[:id] # retrieve movie ID from URI route
-  @movie = Movie.find(id) # look up movie by unique ID
-  # will render app/views/movies/show.html.haml by default
-end
-
-def new
-  @movie = Movie.new
-  # default: render 'new' template
-end 
-
- def create
-    #@movie = Movie.create!(params[:movie]) #old way
-    @movie = Movie.create!(movie_params)  # new way
-    flash[:notice] = "#{@movie.title} was successfully created."
-    redirect_to movies_path
- end
-
-  # add below all other methods
-private
-  def movie_params
-    params.require(:movie).permit(:title, :rating, :description, :release_date)
+  #in app/controllers/movies_controller.rb
+  
+  def show
+    id = params[:id] # retrieve movie ID from URI route
+    @movie = Movie.find(id) # look up movie by unique ID
+    # will render app/views/movies/show.html.haml by default
   end
+  
+  def new
+    @movie = Movie.new
+    # default: render 'new' template
+  end 
+  
+   def create
+      #@movie = Movie.create!(params[:movie]) #old way
+      @movie = Movie.create!(movie_params)  # new way
+      flash[:notice] = "#{@movie.title} was successfully created."
+      redirect_to movies_path
+   end
+  
+    # add below all other methods
+  private
+    def movie_params
+      params.require(:movie).permit(:title, :rating, :description, :release_date)
+    end
+end
